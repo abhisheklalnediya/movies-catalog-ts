627c7ac54753e3a17f805be2439d29946752ca84
Abhishek Lal <abhishek.l@wins.local>
2020-07-27 21:40:01 +0530

    Updates

---
+++ precache-manifest.f9d2c71a39d81d467a6723b095c70e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e52fd4fb11d188a58e71ab8dd19fb08",
+    "url": "/movies-catalog-ts/index.html"
+  },
+  {
+    "revision": "42374429869da3b766e3",
+    "url": "/movies-catalog-ts/static/css/main.8cd9f77a.chunk.css"
+  },
+  {
+    "revision": "b0439cd274aa190eab76",
+    "url": "/movies-catalog-ts/static/js/2.9d0e1ba0.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/movies-catalog-ts/static/js/2.9d0e1ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42374429869da3b766e3",
+    "url": "/movies-catalog-ts/static/js/main.54858305.chunk.js"
+  },
+  {
+    "revision": "13834524777236853c82",
+    "url": "/movies-catalog-ts/static/js/runtime-main.c7342659.js"
+  }
+]);
